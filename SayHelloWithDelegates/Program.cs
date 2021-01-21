e413e50136ce495f9bfb56763a00389616bc8fc4
Johann Grabner <jgrabner@business-tuning.at>
2021-01-21 10:59:28 +0100

    SayHello

---
+++ SayHelloWithDelegates/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace SayHelloWithDelegates
+{
+    delegate string DelegateHello(string g);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            SayHello(Spanish,"F");
+            SayHello(Spanish,"M");
+            SayHello(German,"M");
+        }
+        public static string Spanish(string geschlecht)
+        {
+            string result = "";
+            if (geschlecht=="F")
+            {
+                result = "Hola Sinorita";
+            }
+            else
+            {
+                result = "Hola Hombre";
+            }
+            return result;
+                 
+        }
+        public static string German(string geschlecht)
+        {
+            string result = "";
+            if (geschlecht == "F")
+            {
+                result = "Sehr geehrte Frau";
+            }
+            else
+            {
+                result = "Sehr geehrter Herr";
+            }
+            return result;
+
+        }
+        public static void SayHello(DelegateHello delHello, string geschlecht)
+        {
+            string begruessung = delHello(geschlecht);
+            Console.WriteLine(begruessung);
+        }
+    }
+}
