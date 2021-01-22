c10bfde718d7cdd0d89a9ed7d6e19c98d5a87cbf
Johann Grabner <jgrabner@business-tuning.at>
2021-01-22 10:15:53 +0100

    EF mit .NET Framework

---
+++ ConsoleAppFrameworkMitEF/Model/Produkt.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppFrameworkMitEF.Model
+{
+    class Produkt
+    {
+        //Klasse + ID --> Primarschlüssel
+        public int ProduktID { get; set; }
+        public string Bezeichnung { get; set; }
+        public string Kategorie { get; set; }
+    }
+}
