b14d86c1d9537ddd74f395e278c338efde5da051
Johann Grabner <jgrabner@business-tuning.at>
2021-01-22 10:17:38 +0100

    EF .NET Framework

--- ConsoleAppFrameworkMitEF/Model/ProduktContext.cs
+++ ConsoleAppFrameworkMitEF/Model/ProduktContext.cs
@@ -1,13 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Data.Entity;
+
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ConsoleAppFrameworkMitEF.Model
 {
-    //NuGet EntityFramework
+    //NuGet EntityFramework 
     class ProduktContext: DbContext
     {
         public DbSet<Produkt> Produkte{ get; set; }
