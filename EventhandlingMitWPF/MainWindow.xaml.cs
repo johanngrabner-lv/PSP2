63d9b6d54eb2d302ead29b8d87891010ce17070c
Johann Grabner <jgrabner@business-tuning.at>
2021-02-04 13:54:30 +0100

    WPF - Eventhandling & Casting, Calculator

--- EventhandlingMitWPF/MainWindow.xaml.cs
+++ EventhandlingMitWPF/MainWindow.xaml.cs
@@ -23,15 +23,74 @@ namespace EventhandlingMitWPF
         public MainWindow()
         {
             InitializeComponent();
+
         }
 
+        //Event-Handler
         private void einBegruessungsbutton_Click(object sender, RoutedEventArgs e)
         {
             var eingabe = txtName.Text;
+            CalcBegruessung(eingabe);
+        }
+
+       
+
+        //Event-Handler
+        private void lstGeschlecht_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Produkt p = new Produkt();
+            p.Preis = 12;
+            p.Bezeichnubg = "Handy";
+
+
+
+            var eingabe = txtName.Text;
+            try
+            {
+                /*  <ListBoxItem>
+                <StackPanel>
+                    <Button>Frau</Button>
+                </StackPanel>
+                </ListBoxItem>
+                */
+                var stackPanel = (StackPanel) ((ListBoxItem)lstGeschlecht.SelectedValue).Content;
+
+                var button = (Button) stackPanel.Children[0];
+
+                var inhalt = button.Content;
+            }
+            catch (Exception)
+            {
+
+                ;
+            }
+            var geschlecht = ((ListBoxItem)lstGeschlecht.SelectedValue).Content.ToString(); 
+            CalcBegruessung(eingabe,geschlecht);
+        }
+
+        private void CalcBegruessung(string eingabe)
+        {
             var begr = "Hello " + eingabe;
             Begruessung.Content = begr;
         }
 
-      
+        private void CalcBegruessung(string eingabe, string geschlecht)
+        {
+            string begr = "Sehr ";
+            switch (geschlecht)
+            {
+                case "Frau":
+                    begr += "geehrte Frau ";
+                    break;
+                case "Mann":
+                    begr += "geehrter Herr ";
+                    break;
+                default:
+                    begr = "Hallo ";
+                    break;
+            }
+            begr += eingabe;
+            Begruessung.Content = begr;
+        }
     }
 }
---
+++ EventhandlingMitWPF/Produkt.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventhandlingMitWPF
+{
+    public class Produkt
+    {
+        public string Bezeichnubg { get; set; }
+        public double Preis { get; set; }
+    }
+}
