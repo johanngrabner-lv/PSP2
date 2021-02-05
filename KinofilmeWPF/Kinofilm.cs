dd6224860eb3e319b51b2ea82a645dae2374272d
Johann Grabner <jgrabner@business-tuning.at>
2021-02-05 11:32:33 +0100

    Kinofilm

---
+++ KinofilmeWPF/Kinofilm.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KinofilmeWPF
+{
+    class Kinofilm
+    {
+        public string Titel { get; set; }
+        public string Erscheinungsjahr { get; set; }
+
+        public string Genre { get; set; }
+
+        public double  Sterne { get; set; }
+    }
+}
