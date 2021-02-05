ce5f9dd4801a94f140133969a76bb1edf0480d94
Johann Grabner <jgrabner@business-tuning.at>
2021-02-05 10:40:43 +0100

    Dateneingabe  / Databinding

---
+++ WPFDataBindingModesAndUpdateTrigger/Produkt.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFDataBindingModesAndUpdateTrigger
+{
+    class Produkt
+    {
+        public int ProduktNr { get; set; }
+        public string Produktbezeichnung { get; set; }
+        public double Preis { get; set; }
+    }
+}
