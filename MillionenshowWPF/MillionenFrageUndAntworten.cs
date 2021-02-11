eea1437dea438dab3a3530e3505f48d6c205ecd1
Johann Grabner <jgrabner@business-tuning.at>
2021-02-11 13:59:25 +0100

    Databinding / INotifyPropertyChagned - System.ComponentModel

--- MillionenshowWPF/MillionenFrageUndAntworten.cs
+++ MillionenshowWPF/MillionenFrageUndAntworten.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace MillionenshowWPF
 {
-    class MillionenFrageUndAntworten
+    class MillionenFrageUndAntworten: INotifyPropertyChanged
     {
         public string Frage { get; set; }
 
@@ -15,15 +16,42 @@ namespace MillionenshowWPF
         public string Antwort3 { get; set; }
         public string Antwort4 { get; set; }
 
-        public string Eingabe { get; set; }
+        // public string Eingabe { get; set; }
+
+        private string _Eingabe;
+
+        public string Eingabe
+        {
+            get { return _Eingabe; }
+            set {
+                
+                _Eingabe = value; 
+
+                if (PropertyChanged!=null)
+                {
+                    PropertyChanged(this, 
+                        new PropertyChangedEventArgs("Ergebnis"));
+                }
+            }
+        }
+
 
         public string KorrekteAntwort { get; set; }
 
+        //propfull
         private string _Ergebnis;
+
+        public event PropertyChangedEventHandler PropertyChanged;
 
         public string Ergebnis
         {
-            get { return _Ergebnis; }
+            get {
+                if (Eingabe == KorrekteAntwort)
+                    return "Super, richtig beantwortet";
+                else
+                    return "Leider nicht richtig";
+            
+            }
             set { _Ergebnis = value; }
         }
 
