ed02061c7d9829ec7bc2bbfcd572e7f6532228c0
Johann Grabner <jgrabner@business-tuning.at>
2021-02-11 14:31:15 +0100

    MatheWPF

---
+++ MatheWPF/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MatheWPF
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        //DataContext ctx
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            //DataContext --- z1, z2, eingabe, richtigFalsch,   GesamtRichtig / GesamtFalsch --- 10 von 15 Fragen richtig
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //ctx.NewQuestion();
+        }
+    }
+}
