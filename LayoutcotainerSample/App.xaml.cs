aefd25bb404945131b961c4dd1c1d6d27fa81bc8
Johann Grabner <jgrabner@business-tuning.at>
2021-02-11 12:43:01 +0100

    Layoutcontainer

--- LayoutcotainerSample/App.xaml.cs
+++ LayoutcotainerSample/App.xaml.cs
@@ -6,10 +6,10 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace WpfNet
+namespace LayoutcotainerSample
 {
     /// <summary>
-    /// Interaction logic for App.xaml
+    /// Interaktionslogik für "App.xaml"
     /// </summary>
     public partial class App : Application
     {
--- LayoutcotainerSample/MainWindow.xaml.cs
+++ LayoutcotainerSample/MainWindow.xaml.cs
@@ -13,10 +13,10 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace WpfNet
+namespace LayoutcotainerSample
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaktionslogik für MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
