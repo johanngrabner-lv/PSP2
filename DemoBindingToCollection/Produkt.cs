b8923220c7db615ba2e16f80f9ab7f93a0c28de6
Johann Grabner <jgrabner@business-tuning.at>
2021-02-12 10:47:13 +0100

    ObservableCollection

---
+++ DemoBindingToCollection/Produkt.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoBindingToCollection
+{
+    class Produkt
+    {
+        public int ProduktId { get; set; }
+
+        public string Bezeichnung { get; set; }
+
+        public double Preis { get; set; }
+
+        public string Abbildung { get; set; }
+    }
+}
---
+++ DemoBindingToCollection/ProduktListModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoBindingToCollection
+{
+    class ProduktListModel
+    {
+        //public List<Produkt> MeineProdukte{ get; set; }
+
+        public ObservableCollection<Produkt> MeineProdukte { get; set; }
+    }
+}
