06d484afddedf0f65195b7eb34b3dd93901c16ed
Johann Grabner <jgrabner@business-tuning.at>
2021-02-18 09:23:49 +0100

    M V VM

---
+++ UrlaubWPF/Model/MainWindowViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace UrlaubWPF.Model
+{
+    class MainWindowViewModel
+    {
+        private string _AnzahlUrlaubGesamt;
+
+        public string AnzahlUrlaubGesamt
+        {
+            get { return _AnzahlUrlaubGesamt; }
+            set { _AnzahlUrlaubGesamt = value; }
+        }
+        private string _AnzahlUrlaubAction;
+
+        public string AnzahlUrlaubAction
+        {
+            get { return _AnzahlUrlaubAction; }
+            set { _AnzahlUrlaubAction = value; }
+        }
+        private string _AnzahlUrlaubRelax;
+
+        public string AnzahlUrlaubRelax
+        {
+            get { return _AnzahlUrlaubRelax; }
+            set { _AnzahlUrlaubRelax = value; }
+        }
+
+        public ObservableCollection<Urlaub>
+            ObservableUrlaubsCollection
+        { get; set; }
+
+        public Urlaub AusgeweahlterUrlaub { get; set; }
+
+
+
+    }
+}
---
+++ UrlaubWPF/Model/Urlaub.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace UrlaubWPF.Model
+{
+    class Urlaub: INotifyPropertyChanged
+    {
+        public int UrlaubId { get; set; }
+        private string _Urlaubsart;
+
+        public string Urlaubsart
+        {
+            get { return _Urlaubsart; }
+            set { 
+                _Urlaubsart = value; 
+                if (PropertyChanged!=null)
+                {
+                    PropertyChanged(this, new PropertyChangedEventArgs("Urlaubsart"));
+                }
+            }
+        }
+
+        public string Beschreibung { get; set; }
+        public string Bild { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
