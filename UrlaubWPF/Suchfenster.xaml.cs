a95da7234121e3b6b9db4d9c030c9624b0c04fce
Johann Grabner <jgrabner@business-tuning.at>
2021-02-18 12:37:45 +0100

    Suchfenster

---
+++ UrlaubWPF/Suchfenster.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace UrlaubWPF
+{
+    /// <summary>
+    /// Interaction logic for Suchfenster.xaml
+    /// </summary>
+    public partial class Suchfenster : Window
+    {
+        public Suchfenster()
+        {
+            InitializeComponent();
+
+            this.DataContext = new SuchfensterViewModel();
+        }
+    }
+}
---
+++ UrlaubWPF/SuchfensterViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UrlaubWPF
+{
+    class SuchfensterViewModel
+    {
+        public string Suchtext { get; set; }
+    }
+}
